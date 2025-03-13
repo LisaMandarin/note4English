788eb66580b3abe51a6149efbd4c37585467abd0
Lisa <122068169+LisaMandarin@users.noreply.github.com>
2025-03-14 04:02:06 +0800

    feat: article card

---
+++ src/components/ArticleCard.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+    import { HiOutlineQuestionMarkCircle } from 'vue-icons-plus/hi';
+    import { ref } from 'vue';
+    const title = ref("輸入英文文章")
+</script>
+<template>
+    <h2 class="text-center text-2xl font-extrabold font-chinese">
+        {{ title }}
+        <HiOutlineQuestionMarkCircle class="inline"/>
+    </h2>
+    <div class="px-8">
+        <textarea placeholder="請貼上英文文章" class="border w-full p-2"></textarea>
+    </div>
+</template>
---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+    import { ref } from 'vue';
+    const siteName = ref("note4English")
+    const thisYear = new Date().getFullYear();
+    const author = ref("Min-ting Chuang")
+</script>
+
+<template>
+    <div class="space-x-2 w-fit mx-auto py-4">
+        <span>{{ siteName }}</span>
+        <span>&copy;{{ thisYear }}</span>
+        <span>{{ author }}</span>
+    </div>
+</template>
