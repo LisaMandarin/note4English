a8c7db7b2c92fbc906152d0111e3e8276abd62c7
Lisa <122068169+LisaMandarin@users.noreply.github.com>
2025-03-15 02:44:34 +0800

    feat: article buttons

--- src/components/ArticleCard.vue
+++ src/components/ArticleCard.vue
@@ -1,14 +1,34 @@
 <script lang="ts" setup>
-    import { HiOutlineQuestionMarkCircle } from 'vue-icons-plus/hi';
-    import { ref } from 'vue';
-    const title = ref("輸入英文文章")
+import { HiOutlineQuestionMarkCircle } from "vue-icons-plus/hi";
+import { ref } from "vue";
+
+const title = ref("輸入英文文章");
+const textareaValue = ref<string>("");
 </script>
 <template>
-    <h2 class="text-center text-2xl font-extrabold font-chinese">
-        {{ title }}
-        <HiOutlineQuestionMarkCircle class="inline"/>
-    </h2>
-    <div class="px-8">
-        <textarea placeholder="請貼上英文文章" class="border w-full p-2"></textarea>
-    </div>
+  <h2 class="text-center text-2xl font-extrabold font-chinese">
+    {{ title }}
+    <HiOutlineQuestionMarkCircle class="inline" />
+  </h2>
+  <div class="px-8">
+    <a-textarea
+      v-model:value="textareaValue"
+      placeholder="請貼上英文文章"
+      :rows="8"
+      autosize
+      allowClear
+      maxlength="1000"
+      showCount
+      id="articleTextarea"
+    />
+  </div>
+  <div class="flex w-full gap-4 justify-center pt-4">
+    <button
+      @click="textareaValue = ''"
+      class="btnSecondary text-lg"
+    >
+      清除文字
+    </button>
+    <button class="btnPrimary text-lg">陳列句子</button>
+  </div>
 </template>
